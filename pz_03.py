8546ffc8e8540c90a696c0e4de29b919934645c0
RomaNiceMustache <riorbas@yandex.by>
2022-11-27 12:37:59 +0300

    03

--- pz_03.py
+++ pz_03.py
@@ -9,14 +9,24 @@ class Tomato:
     def grow(self):
         self._index += 1
         self._state = self.growing["states"][self._index]
-        self.is_ripe()
 
     def is_ripe(self):
-        if self._state == self.growing["states"][2]:
-            print("good")
-        else:
-            self.grow()
+        return self._state == self.growing["states"][2]
 
+
+class TomatoBush:
+
+    def __init__(self, tomatoes):
+        pass
+
+    def graw_all(self):
+        Tomato(self._index)
+
+    def all_are_ripe(self):
+        return Tomato.is_ripe()
+
+    def give_away_all(self):
+        pass
 
 test = Tomato(0)
 print(test)
